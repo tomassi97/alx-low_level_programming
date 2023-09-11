4c265976ca1fb81a9674f5efdbca0d2a4ebd6d5a
tomassi97 <thomasmithamo99@gmail.com>
2023-09-11 10:56:27 -0700

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,15 +5,22 @@
  *
  * Return: Always 0 (Succes)
  */
-int main(void)
-{
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
+int main(void) {
+    char lowercase = 'a';
+    char uppercase = 'A';
 
-	for (i = 0; i < 52; i++)
-	{
-		putchar(alp[alp[i]);
-	}
-	putchar('\n');
-	return (0);
+    while (lowercase <= 'z') {
+        putchar(lowercase);
+        lowercase++;
+    }
+
+    while (uppercase <= 'Z') {
+        putchar(uppercase);
+        uppercase++;
+    }
+
+    putchar('\n');
+
+    return 0;
 }
+
