6e949145af9f6db19ab7fb33479912f40042fa11
tomassi97 <thomasmithamo99@gmail.com>
2023-09-11 04:51:08 -0700

    8 print

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,18 +5,19 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	int i;
+int main(void) {
+    char c;
 
-	for (i = 48; i < 58; i++)
-	{
-		putchar(i);
-	}
-	for (i = 97; i < 103; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
-	return (0)
+    for (c = '0'; c <= '9'; c++) {
+        putchar(c);
+    }
+
+    for (c = 'a'; c <= 'f'; c++) {
+        putchar(c);
+    }
+
+    putchar('\n');
+
+    return (0);
 }
+
