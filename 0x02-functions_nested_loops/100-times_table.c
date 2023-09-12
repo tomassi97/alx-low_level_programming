dcbc990af130c96c3d0956d338250824f31f10c2
tomassi97 <thomasmithamo99@gmail.com>
2023-09-12 10:13:25 -0700

    100 times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,17 +27,19 @@ void print_times_table(int n)
 
 					if (result < 10)
 					{
-						_putchar(' ');
-						_putchar(' ');
+						_putchar(' '); // Extra space added here
 					}
 					else if (result < 100)
 					{
 						_putchar(' ');
 					}
 
-					_putchar(result / 100 + '0');
-					_putchar((result / 10) % 10 + '0');
-					_putchar(result % 10 + '0');
+					if (result < 10)
+					{
+						_putchar(' '); // Extra space added here
+					}
+
+					_putchar(result + '0');
 				}
 			}
 			_putchar('\n');
