d7b70644587e4dc07000a36eb1441db66d72fc99
tomassi97 <thomasmithamo99@gmail.com>
2023-09-12 02:02:12 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphabet
+ *
+ * Return:void
+ */
+
+void print_alphabe_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
