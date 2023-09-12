03772c98fa47f1b9a39787eda6bb6070ae3b15d4
tomassi97 <thomasmithamo99@gmail.com>
2023-09-12 10:08:14 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,26 +15,28 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				result = i * j;
+
 				if (j == 0)
+				{
 					_putchar('0');
+				}
 				else
 				{
 					_putchar(',');
 					_putchar(' ');
 
 					if (result < 10)
+					{
 						_putchar(' ');
+						_putchar(' ');
+					}
+					else if (result < 100)
+					{
+						_putchar(' ');
+					}
 
-					if (result >= 100)
-					{
-						_putchar(result / 100 + '0');
-						_putchar((result / 10) % 10 + '0');
-					}
-					else if (result >= 10)
-					{
-						_putchar(' ');
-						_putchar(result / 10 + '0');
-					}
+					_putchar(result / 100 + '0');
+					_putchar((result / 10) % 10 + '0');
 					_putchar(result % 10 + '0');
 				}
 			}
