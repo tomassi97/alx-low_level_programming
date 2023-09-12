8dc18c56665c339cb851d8621764934f2f185650
tomassi97 <thomasmithamo99@gmail.com>
2023-09-12 04:04:17 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,24 +8,8 @@
  */
 int _islower(int c)
 {
-	return (c >= 'a' && c <= 'z');
-}
-
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+    if (c >= 'a' && c <= 'z')
+        return (1);
+    else
+        return (0);
 }
