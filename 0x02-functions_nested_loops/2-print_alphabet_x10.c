e5e00577d76e8970f1abc9605d6f0655c4137ac0
tomassi97 <thomasmithamo99@gmail.com>
2023-09-12 03:28:19 -0700

    part 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+    int i, j;
+    char letter;
+
+    for (i = 0; i < 10; i++)
+    {
+        letter = 'a';
+        for (j = 0; j < 26; j++)
+        {
+            _putchar(letter);
+            letter++;
+        }
+        _putchar('\n');
+    }
+}
+
