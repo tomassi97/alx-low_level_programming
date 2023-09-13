ddb0109335edc407616c0cf3d083fb773b934fbf
tomassi97 <thomasmithamo99@gmail.com>
2023-09-13 03:27:59 -0700

    task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,5 @@
 #ifendf MAIN_H
-#fifine MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
@@ -8,4 +8,4 @@ int largest_number(int a, int b, int c);
 int convert_days(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-#end if
+#endif
