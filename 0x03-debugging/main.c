abdac367606777f69408e83720087c75b51521c7
tomassi97 <thomasmithamo99@gmail.com>
2023-09-14 00:36:03 -0700

    task 0

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * This function tests the positive_or_negative function with a positive integer.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
