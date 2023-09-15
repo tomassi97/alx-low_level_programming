64893b4ff3192e6ea74811519d7623f7055272e0
tomassi97 <thomasmithamo99@gmail.com>
2023-09-15 06:55:25 -0700

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,18 +6,17 @@
  */
 void print_square(int size)
 {
-    int i, j;
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
 
-    if (size <= 0)
-        _putchar('\n');
-    else
-    {
-        for (i = 0; i < size; i++)
-        {
-            for (j = 0; j < size; j++)
-                _putchar('#');
-            _putchar('\n');
-        }
-    }
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
 }
-
