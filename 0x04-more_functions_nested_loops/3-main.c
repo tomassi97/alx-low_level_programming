9c6d24ff4055e7fd8cdb333d3e32b78b9151d6dc
tomassi97 <thomasmithamo99@gmail.com>
2023-09-15 11:38:54 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-main.c
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -1,12 +1,8 @@
 #include "main.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
-    print_numbers();
-    return (0);
+    int number = 123;
+    print_number(number);
+    return 0;
 }
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints numbers from 0 to 9 followed by a new line.
+ * print_numbers - print numbers from 0 to 9
+ * Return:ALways 0
  */
+
 void print_numbers(void)
 {
-    int i;
+	char c;
 
-    for (i = 0; i <= 9; i++)
-    {
-        _putchar(i + '0');
-    }
-
-    _putchar('\n');
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
 }
