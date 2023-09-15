d489def5e898f0da2138265f022c18c6ee4a92ed
tomassi97 <thomasmithamo99@gmail.com>
2023-09-15 10:02:43 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Prints numbers from 0 to 9, excluding 2 and 4.
+ * print_most_numbers - Prints numbers from 0 to 9 excluding 2 and 4.
  */
 void print_most_numbers(void)
 {
@@ -10,10 +10,7 @@ void print_most_numbers(void)
     for (i = 0; i <= 9; i++)
     {
         if (i != 2 && i != 4)
-        {
             _putchar(i + '0');
-        }
     }
-
     _putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void print_square(int size);
+void print_most_numbers(void);
 int _putchar(char c);
 
 #endif
