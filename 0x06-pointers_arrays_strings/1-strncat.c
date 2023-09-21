1fbb88e3a29aa42f0c5e5c7544ad8280fcc3f220
tomassi97 <thomasmithamo99@gmail.com>
2023-09-21 00:46:48 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,28 +1,18 @@
 #include "main.h"
-
 /**
- * _strncat - concantenates two strings.
- * @dest: arg to concantinate to.
- * @src: arg to concantinate from.
- * @n: arg to poitn out number of bytes.
- * Return: (dest) poiner to original start dest.
-*/
-
-char *_strncat(char *dest, char *src, int n)
+ * _strncpy - concatenate strings, defining the size of second string.
+ * @dest: string with concatenation
+ * @src: string to be concatenated
+ * @n: size of second string
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
 {
-	char *home = dest;
-	char *away = src;
+	int i;
 
-
-	while (*away != '\0' && n > 0)
-	{
-		*home = *away;
-		home++;
-		away++;
-		n--;
-	}
-
-	*home = '\0';
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		*(dest + i) = *(src + i);
+	for ( ; i < n; i++)
+		*(dest + i) = '\0';
 	return (dest);
-
 }
