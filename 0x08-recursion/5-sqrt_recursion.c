b955291ec774e276815c49f7323028a6eb24e415
tomassi97 <thomasmithamo99@gmail.com>
2023-09-28 12:45:25 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _sqrt - Finds the square root of a value.
+ * @prev: The previous value.
+ * @root: The square value.
+ *
+ * Return: The square root.
+ */
+int _sqrt(int prev, int root)
+{
+    if (prev > root)
+    {
+        return (-1);
+    }
+    else if (prev * prev == root)
+    {
+        return (prev);
+    }
+
+    return (_sqrt(prev + 1, root));
+}
+
+/**
+ * _sqrt_recursion - Recursive square root of a number.
+ * @n: The integer.
+ *
+ * Return: Square root.
+ */
+int _sqrt_recursion(int n)
+{
+    if (n < 0)
+    {
+        return (-1);
+    }
+
+    return (_sqrt(1, n));
+}
