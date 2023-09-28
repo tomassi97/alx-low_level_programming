c7fd72ccdebc1187995d4a782d5430bf809cef29
tomassi97 <thomasmithamo99@gmail.com>
2023-09-28 12:48:48 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,16 +9,16 @@
  */
 int _sqrt(int prev, int root)
 {
-    if (prev > root)
-    {
-        return (-1);
-    }
-    else if (prev * prev == root)
-    {
-        return (prev);
-    }
+	if (prev > root)
+	{
+		return (-1);
+	}
+	else if (prev * prev == root)
+	{
+		return (prev);
+	}
 
-    return (_sqrt(prev + 1, root));
+	return (_sqrt(prev + 1, root));
 }
 
 /**
@@ -29,10 +29,10 @@ int _sqrt(int prev, int root)
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-    {
-        return (-1);
-    }
+	if (n < 0)
+	{
+		return (-1);
+	}
 
-    return (_sqrt(1, n));
+	return (_sqrt(1, n));
 }
