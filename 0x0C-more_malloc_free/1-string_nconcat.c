1e7a99d3e7b571d8b88d6fbd1f66f88cad6cb24f
tomassi97 <thomasmithamo99@gmail.com>
2023-10-05 03:52:20 -0700

    tasks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,54 +2,45 @@
 #include "main.h"
 
 /**
- * string_nconcat - Concatenates two strings up to n characters from s2.
- * @s1: The first string
- * @s2: The second string
- * @n: The maximum number of characters to concatenate from s2
+ * string_nconcat - function that concatenates two strings
+ * @s1: value 1
+ * @s2: value 2
+ * @n: value 3
  *
- * Return: A pointer to a newly allocated space in memory
- *         containing the concatenated string,
- *         or NULL if allocation fails
+ * Return: pointer
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *result;
-    unsigned int i = 0, j = 0, k;
+	char *point;
+	unsigned int i = 0, j = 0, k;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
-
-    // Calculate the length of s1
-    while (s1[i])
-        i++;
-
-    // Calculate the length of s2
-    while (s2[j])
-        j++;
-
-    // Limit n to the length of s2
-    if (n >= j)
-        n = j;
-
-    j = 0;
-
-    // Allocate memory for the concatenated string
-    result = malloc(sizeof(char) * (i + n + 1));
-    if (result == NULL)
-        return (NULL);
-
-    // Copy characters from s1 to the result
-    for (k = 0; k < i; k++)
-        result[k] = s1[k];
-
-    // Concatenate characters from s2 up to n
-    for (; k < (i + n); k++)
-        result[k] = s2[k - i];
-
-    // Null-terminate the concatenated string
-    result[k] = '\0';
-
-    return (result);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i])
+	{
+		i++;
+	}
+	while (s2[j])
+	{
+		j++;
+	}
+	if (n >= j)
+		n = j;
+	j = 0;
+	point = malloc(sizeof(char) * (i + n + 1));
+	if (point == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+	{
+		point[k] = s1[k];
+	}
+	for (; k < (i + n); k++)
+	{
+		point[k] = s2[k - i];
+	}
+	point[k] = '\0';
+	return (point);
 }
